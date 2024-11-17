6435e4a133fbcc4bc1ec5797780e752c59952f53
Seeryyds <158558757+Seeryyds@users.noreply.github.com>
2024-11-17 22:45:48 +0100

    Update soupX.sh

--- soupX.sh
+++ soupX.sh
@@ -1,17 +1,22 @@
 #!/bin/bash 
-#SBATCH -A naiss2024-22-757         # 项目编号
-#SBATCH -p main                     # 分区名称
-#SBATCH -t 0-12:00:00               # 设置运行的最大时间
-#SBATCH -n 24                       # 使用的CPU核心数（根据需要调整）
-#SBATCH -J soupx_process            # 作业名称
-#SBATCH -e soupx_process.err        # 错误输出文件
-#SBATCH -o soupx_process.out        # 标准输出文件
-#SBATCH --mail-user=xuanyil@kth.se  # 邮箱地址
-#SBATCH --mail-type=ALL             # 通知类型（开始、结束、失败等）
+#SBATCH -A naiss2024-22-757
+#SBATCH -p main
+#SBATCH -t 0-12:00:00
+#SBATCH -n 2
+#SBATCH -J soupx_process
+#SBATCH -e soupx_process.err
+#SBATCH -o soupx_process.out
+#SBATCH --mail-user=xuanyil@kth.se
+#SBATCH --mail-type=ALL
 
-# 加载R模块，确保R环境已安装SoupX、Seurat等依赖包
-module load PDCOLD/23.03
-module load R/4.3.2
-# 运行R脚本
-Rscript /cfs/klemming/projects/snic/rnaatlas/nobackup/private/xuanyi/Rscript/process_samples.R
+# 加载 Conda
+source ~/miniconda3/etc/profile.d/conda.sh
 
+# 激活 Conda 环境
+conda activate r_env
+
+# 运行 R 脚本
+Rscript /cfs/klemming/projects/supr/rnaatlas/nobackup/private/xuanyi/process_samples.R
+
+# 结束后停用 Conda 环境
+conda deactivate
